ec57579b6a4fc81e0804c40098cdef058873ef18
krupiceva <dora.mlinaric94@gmail.com>
2021-12-13 20:35:33 +0100

    Enable block editor

--- academy-post-types.php
+++ academy-post-types.php
@@ -2,6 +2,7 @@
 function academy_post_types(){
 	register_post_type('event', array(
 		'public' => true,
+		'show_in_rest' => true,
 		'labels' => array(
 			'name' => 'Events',
 			'add_new_item' => 'Add New Event',
